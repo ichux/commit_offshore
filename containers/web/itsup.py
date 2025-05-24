3a2bdd31f9e76d30a455bb0b877325b7a48cbb22
Chukwudi Nwachukwu <theichux@gmail.com>
2025-05-24 10:26:00 +0100

    simplified as expected

--- containers/web/itsup.py
+++ containers/web/itsup.py
@@ -3,12 +3,12 @@ import socket
 import time
 import os
 
-PORT=16432
+PORT = 16432
 
 while True:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.settimeout(1)
-        result = sock.connect_ex(('127.0.0.1', PORT))
+        result = sock.connect_ex(("127.0.0.1", PORT))
         if result == 0:
             print(f"+ {PORT}")
             break
